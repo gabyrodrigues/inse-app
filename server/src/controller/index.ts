94dda63122d547af88be4c84552addb26a79b2fc
gabyrodrigues <gabyrlima.99@gmail.com>
2024-01-20 18:00:09 -0300

    adding search routes * functions + adjust types

--- server/src/controller/index.ts
+++ server/src/controller/index.ts
@@ -14,32 +14,57 @@ const uploadOpts = {
 };
 
 interface InseDataItem {
-  NU_ANO_SAEB: number;
-  CO_UF: number;
-  SG_UF: string;
-  NO_UF: string;
-  CO_MUNICIPIO: string;
-  NO_MUNICIPIO: string;
-  ID_ESCOLA: string;
-  NO_ESCOLA: string;
-  TP_TIPO_REDE: number;
-  TP_LOCALIZACAO: number;
-  TP_CAPITAL: number;
-  QTD_ALUNOS_INSE: number;
-  MEDIA_INSE: number;
-  INSE_CLASSIFICACAO: string;
-  PC_NIVEL_1: number;
-  PC_NIVEL_2: number;
-  PC_NIVEL_3: number;
-  PC_NIVEL_4: number;
-  PC_NIVEL_5: number;
-  PC_NIVEL_6: number;
-  PC_NIVEL_7: number;
-  PC_NIVEL_8: number;
+  nu_ano_saeb: number;
+  co_uf: number;
+  sg_uf: string;
+  no_uf: string;
+  co_municipio: string;
+  no_municipio: string;
+  id_escola: string;
+  no_escola: string;
+  tp_tipo_rede: number;
+  tp_localizacao: number;
+  tp_capital: number;
+  qtd_alunos_inse: number;
+  media_inse: number;
+  inse_classificacao: string;
+  pc_nivel_1: number;
+  pc_nivel_2: number;
+  pc_nivel_3: number;
+  pc_nivel_4: number;
+  pc_nivel_5: number;
+  pc_nivel_6: number;
+  pc_nivel_7: number;
+  pc_nivel_8: number;
 }
 
-const uploadExcel = async function (req: Request, res: Response) {
-  res.render("uploadexcel", { text: "Upload Excel" });
+const transformExcelData = (excelData: any): InseDataItem => {
+  const transformedData: InseDataItem = {
+    nu_ano_saeb: excelData?.NU_ANO_SAEB,
+    co_uf: excelData?.CO_UF,
+    sg_uf: excelData?.SG_UF,
+    no_uf: excelData?.NO_UF,
+    co_municipio: String(excelData?.CO_MUNICIPIO),
+    no_municipio: excelData?.NO_MUNICIPIO,
+    id_escola: String(excelData?.ID_ESCOLA),
+    no_escola: excelData?.NO_ESCOLA,
+    tp_tipo_rede: excelData?.TP_TIPO_REDE,
+    tp_localizacao: excelData?.TP_LOCALIZACAO,
+    tp_capital: excelData?.TP_CAPITAL,
+    qtd_alunos_inse: excelData?.QTD_ALUNOS_INSE,
+    media_inse: excelData?.MEDIA_INSE,
+    inse_classificacao: excelData?.INSE_CLASSIFICACAO,
+    pc_nivel_1: excelData?.PC_NIVEL_1,
+    pc_nivel_2: excelData?.PC_NIVEL_2,
+    pc_nivel_3: excelData?.PC_NIVEL_3,
+    pc_nivel_4: excelData?.PC_NIVEL_4,
+    pc_nivel_5: excelData?.PC_NIVEL_5,
+    pc_nivel_6: excelData?.PC_NIVEL_6,
+    pc_nivel_7: excelData?.PC_NIVEL_7,
+    pc_nivel_8: excelData?.PC_NIVEL_8
+  };
+
+  return transformedData;
 };
 
 const excelData = async function (req: Request, res: Response) {
@@ -55,35 +80,14 @@ const excelData = async function (req: Request, res: Response) {
 
     const workbook = xlsx.readFile(excel.tempFilePath);
     const sheetName = workbook.SheetNames[0];
-    const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]) as InseDataItem[];
+
+    const rawData: Record<string, string>[] = xlsx.utils.sheet_to_json(
+      workbook.Sheets[sheetName]
+    ) as any[];
+    const data = rawData.map(transformExcelData);
 
     for (const item of data) {
-      await prismaClient.inseData.create({
-        data: {
-          nu_ano_saeb: item.NU_ANO_SAEB,
-          co_uf: item.CO_UF,
-          sg_uf: item.SG_UF,
-          no_uf: item.NO_UF,
-          co_municipio: String(item.CO_MUNICIPIO),
-          no_municipio: item.NO_MUNICIPIO,
-          id_escola: String(item.ID_ESCOLA),
-          no_escola: item.NO_ESCOLA,
-          tp_tipo_rede: item.TP_TIPO_REDE,
-          tp_localizacao: item.TP_LOCALIZACAO,
-          tp_capital: item.TP_CAPITAL,
-          qtd_alunos_inse: item.QTD_ALUNOS_INSE,
-          media_inse: item.MEDIA_INSE,
-          inse_classificacao: item.INSE_CLASSIFICACAO,
-          pc_nivel_1: item.PC_NIVEL_1,
-          pc_nivel_2: item.PC_NIVEL_2,
-          pc_nivel_3: item.PC_NIVEL_3,
-          pc_nivel_4: item.PC_NIVEL_4,
-          pc_nivel_5: item.PC_NIVEL_5,
-          pc_nivel_6: item.PC_NIVEL_6,
-          pc_nivel_7: item.PC_NIVEL_7,
-          pc_nivel_8: item.PC_NIVEL_8
-        }
-      });
+      await prismaClient.inseData.create({ data: item });
     }
 
     fs.unlinkSync(excel.tempFilePath);
@@ -104,11 +108,11 @@ const listInseData = async function (req: Request, res: Response) {
 
   const [data, total] = await prismaClient.$transaction([
     prismaClient.inseData.findMany({
-      skip,
-      take,
       orderBy: {
         no_escola: "asc"
-      }
+      },
+      skip,
+      take
     }),
     prismaClient.inseData.count()
   ]);
@@ -119,7 +123,7 @@ const listInseData = async function (req: Request, res: Response) {
     data: {
       total,
       totalOfPages,
-      data
+      result: data
     }
   });
 };
@@ -127,20 +131,141 @@ const listInseData = async function (req: Request, res: Response) {
 const findSchoolInseData = async function (req: Request, res: Response) {
   const { id } = req.params;
 
-  const data = await prismaClient.inseData.findUniqueOrThrow({
-    where: {
-      id
-    }
-  });
+  try {
+    const data = await prismaClient.inseData.findUniqueOrThrow({
+      where: {
+        id
+      }
+    });
 
-  return res.status(200).json({ data });
+    return res.status(200).json({ data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error finding data" });
+  }
+};
+
+const searchData = async function (req: Request, res: Response) {
+  const { search } = req.query;
+
+  const page = req.query.page ? +req.query.page : 1;
+  const pageSize = 100;
+
+  const skip = (page - 1) * pageSize;
+  const take = pageSize;
+
+  try {
+    const [data, total] = await prismaClient.$transaction([
+      prismaClient.inseData.findMany({
+        where: {
+          OR: [
+            { no_escola: { contains: String(search) } },
+            { no_municipio: { contains: String(search) } },
+            { no_uf: { contains: String(search) } }
+          ]
+        },
+        skip,
+        take
+      }),
+      prismaClient.inseData.count({
+        where: {
+          OR: [
+            { no_escola: { contains: String(search) } },
+            { no_municipio: { contains: String(search) } },
+            { no_uf: { contains: String(search) } }
+          ]
+        }
+      })
+    ]);
+
+    const totalOfPages = Math.ceil(total / take);
+
+    return res.status(200).json({
+      data: {
+        total,
+        totalOfPages,
+        result: data
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error searching data" });
+  }
+};
+
+interface FilterWhereParams {
+  sg_uf?: string;
+  qtd_alunos_inse?: number | { lte: number };
+  inse_classificacao?: string;
+  tp_tipo_rede?: { in: number[] };
+  order?: string;
+}
+
+const filterData = async function (req: Request, res: Response) {
+  const { sg_uf, qtd_alunos_inse, inse_classificacao, tp_tipo_rede, order } = req.query;
+
+  const page = req.query.page ? +req.query.page : 1;
+  const pageSize = 100;
+
+  const skip = (page - 1) * pageSize;
+  const take = pageSize;
+
+  const where: FilterWhereParams = {};
+
+  if (sg_uf) {
+    where.sg_uf = String(sg_uf);
+  }
+
+  if (qtd_alunos_inse) {
+    where.qtd_alunos_inse = { lte: +qtd_alunos_inse };
+  }
+
+  if (tp_tipo_rede) {
+    where.tp_tipo_rede = {
+      in: String(tp_tipo_rede)
+        .split(",")
+        .map((value: string) => +value.trim())
+    };
+  }
+
+  if (inse_classificacao) {
+    where.inse_classificacao = String(inse_classificacao);
+  }
+
+  try {
+    const [data, total] = await prismaClient.$transaction([
+      prismaClient.inseData.findMany({
+        // where,
+        skip,
+        take,
+        orderBy: {
+          no_escola: order === "asc" ? "asc" : "desc"
+        }
+      }),
+      prismaClient.inseData.count({ where })
+    ]);
+
+    const totalOfPages = Math.ceil(total / take);
+
+    return res.status(200).json({
+      data: {
+        total,
+        totalOfPages,
+        result: data
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error filtering data" });
+  }
 };
 
 export default {
-  uploadExcel,
   excelData,
   listInseData,
   findSchoolInseData,
+  searchData,
+  filterData,
   upload,
   uploadOpts
 };
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,9 +4,10 @@ const route = express.Router();
 
 import controller from "./controller";
 
+route.get("/inse/filter", controller.filterData);
 route.get("/inse/list", controller.listInseData);
 route.get("/inse/:id", controller.findSchoolInseData);
-// route.post("/excel-data", controller.upload.single("excel"), controller.excelData);
+route.get("/inse", controller.searchData);
 route.post("/import-excel", fileUpload(controller.uploadOpts), controller.excelData);
 
 route.all("/*", function (req, res) {
