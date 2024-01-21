ae4055af6505484e02ccd47fe468e57c952d10db
gabyrodrigues <gabyrlima.99@gmail.com>
2024-01-21 18:57:05 -0300

    updating env check

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = import.meta.env.VITE_NODE_ENV === "development";
 
 export const api = axios.create({
   baseURL: isDev ? "http://localhost:3333" : "https://inse-app-api.onrender.com"
